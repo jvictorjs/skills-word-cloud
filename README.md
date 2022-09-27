d76d5967c8a49dc606e397050978714ad410ffed
jvictorhub <jvwr.cin@gmail.com>
2022-09-27 13:49:38 -0300

    v0.1.0 initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Work in progress...👨‍💻
+
+Web page to show my dev skills.
+
+Built with VUE.JS and Vuetify UI library.
---
+++ techs-data.js
@@ -0,0 +1,73 @@
+export { TECHS };
+
+const JVICTOR_JS_TEXT_MENTION_HTML = '<a href="https://github.com/jvictorjs" target="_blank"><strong class="blue--text">@jvictorjs</strong></a>';
+
+const TECHS = [
+  { classType: 4, top: "28%", left: "78%", isPresentInThisProject: true, name: "Material Design Icons" },
+  { classType: 5, top: "8%", left: "77%", isPresentInThisProject: false, name: "Canvas" },
+  { classType: 5, top: "16%", left: "80%", isPresentInThisProject: false, name: "Audio" },
+  { classType: 5, top: "10%", left: "86%", isPresentInThisProject: false,  name: "Markdown" },
+
+  
+  { classType: 2, top: "62%", left: "34%", isPresentInThisProject: false,  name: "REST APIs" },
+  { classType: 4, top: "75%", left: "35%", isPresentInThisProject: false,  name: "Twitter API" },
+  { classType: 3, top: "69%", left: "34%", isPresentInThisProject: false,  name: "Telegram Bots API" },
+  { classType: 5, top: "66%", left: "23%", isPresentInThisProject: false,  name: "HTTP Methods" },
+  { classType: 4, top: "56%", left: "35%", isPresentInThisProject: false,  name: "JSON Server" },
+  
+  { classType: 3, top: "9%", left: "2%", isPresentInThisProject: false,  name: "Google Sheets" },
+  { classType: 2, top: "16%", left: "8%", isPresentInThisProject: false,  name: "Google Apps Script" },
+  { classType: 5, top: "23%", left: "23%", isPresentInThisProject: false,  name: "Triggers" },
+  { classType: 5, top: "11%", left: "17%", isPresentInThisProject: false,  name: "Automation" },
+  { classType: 5, top: "4%", left: "11%", isPresentInThisProject: false,  name: "Custom Menus" },
+  { classType: 5, top: "11%", left: "29%", isPresentInThisProject: false,  name: "Web Apps" },
+  
+
+  { classType: 5, top: "35%", left: "40%", isPresentInThisProject: false,  name: "DOM Manipulation" },
+  { classType: 3, top: "46%", left: "9%", isPresentInThisProject: false,  name: "Node.js" },
+  { classType: 4, top: "55%", left: "18%", isPresentInThisProject: false,  name: "Express.js" },
+  { classType: 3, top: "32%", left: "8%", isPresentInThisProject: false,  name: "Chart.js" },
+  { classType: 3, top: "26%", left: "39%", isPresentInThisProject: false,  name: "TypeScript" },
+  { classType: 5, top: "48%", left: "28%", isPresentInThisProject: false,  name: "Axios" },
+  
+  { classType: 4, top: "30%", left: "56%", isPresentInThisProject: false,  name: "Angular" },
+
+  { classType: 3, top: "9%", left: "43%", isPresentInThisProject: false,  name: "React Native" },
+  { classType: 2, top: "14%", left: "52%", isPresentInThisProject: false,  name: "React" },
+  { classType: 5, top: "4%", left: "40%", isPresentInThisProject: false,  name: "Expo" },
+  { classType: 5, top: "4%", left: "53%", isPresentInThisProject: false,  name: "Android Studio" },
+
+  { classType: 2, top: "43%", left: "57%", isPresentInThisProject: true,  name: "Vue" },
+  { classType: 3, top: "49%", left: "62%", isPresentInThisProject: true,  name: "Vuetify" },
+  { classType: 5, top: "43%", left: "52%", isPresentInThisProject: false,  name: "Vuex" },
+  { classType: 5, top: "50%", left: "51%", isPresentInThisProject: false,  name: "Vue router" },
+
+  { classType: 4, top: "46%", left: "83%", isPresentInThisProject: false,  name: "Tailwind CSS" },
+  { classType: 5, top: "65%", left: "86%", isPresentInThisProject: false,  name: "SASS" },
+  { classType: 5, top: "44%", left: "75%", isPresentInThisProject: false,  name: "Flexbox" },
+  { classType: 5, top: "66%", left: "73%", isPresentInThisProject: false,  name: "Animations" },
+
+  { classType: 4, top: "79%", left: "50%", isPresentInThisProject: false,  name: "GraphQL" },
+
+  { classType: 3, top: "91%", left: "62%", isPresentInThisProject: false,  name: "Pentaho Data Integration" },
+  { classType: 5, top: "86%", left: "88%", isPresentInThisProject: false,  name: "Power BI" },
+  { classType: 5, top: "91%", left: "90%", isPresentInThisProject: false,  name: "QlikSense" },
+  { classType: 2, top: "83%", left: "63%", isPresentInThisProject: false,  name: "SQL" },
+  { classType: 5, top: "84%", left: "80%", isPresentInThisProject: false,  name: "SQLite" },
+  { classType: 5, top: "84%", left: "71%", isPresentInThisProject: false,  name: "SQL Server" },
+
+  { classType: 5, top: "87%", left: "40%", isPresentInThisProject: true,  name: "Git" },
+  { classType: 3, top: "91%", left: "43%", isPresentInThisProject: true,  name: "GitHub" },
+
+  { classType: 5, top: "64%", left: "11%", isPresentInThisProject: false,  name: "Socket.io" },
+
+  { classType: 5, top: "53%", left: "8%", isPresentInThisProject: false,  name: "NPM" },
+  { classType: 5, top: "77%", left: "21%", isPresentInThisProject: true,  name: "NGINX" },
+  { classType: 3, top: "77%", left: "6%", isPresentInThisProject: true,  name: "Ubuntu Linux" },
+  { classType: 5, top: "82%", left: "3%", isPresentInThisProject: false,  name: "Vim" },
+  { classType: 5, top: "71%", left: "9%", isPresentInThisProject: true,  name: "PuTTy" },
+  { classType: 4, top: "93%", left: "27%", isPresentInThisProject: false,  name: "Netlify" },
+  { classType: 3, top: "85%", left: "8%", isPresentInThisProject: true,  name: "Digital Ocean" },
+  { classType: 5, top: "83%", left: "22%", isPresentInThisProject: true,  name: "Certbot SSL" },
+  { classType: 5, top: "72%", left: "3%", isPresentInThisProject: false,  name: "PM2" },
+];
